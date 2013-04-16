fd50b1296ba22de27a4ca7271b416e725490edc5
Sahil Talwar <sahiltalwar88@gmail.com>
2013-04-16 19:46:31 -0400

    - Added missing files.

---
+++ Excella.Lean.Domain/Shared/IRepositoryStore.cs
@@ -0,0 +1,15 @@
+﻿namespace Excella.Lean.Domain.Shared
+{
+    using Excella.Lean.Core;
+
+    public interface IRepositoryStore
+    {
+        IEntity Add(IEntity entity);
+
+        IEntity Update(IEntity entity);
+
+        bool Delete(IEntity entity);
+
+        void SaveAllChanges();
+    }
+}
---
+++ Excella.Lean.Domain/Shared/RepositoryStore.cs
@@ -0,0 +1,27 @@
+﻿namespace Excella.Lean.Domain.Shared
+{
+    using Excella.Lean.Core;
+
+    public class RepositoryStore : IRepositoryStore
+    {
+        public IEntity Add(IEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEntity Update(IEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(IEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveAllChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
