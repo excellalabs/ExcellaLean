017c6b5abaf9220f00d8a71f2eb65efd5326dced
Shahed Chowdhuri <shahedc@gmail.com>
2013-04-16 20:36:27 -0400

    re-added Configuration.cs via Enable-Migrations, cleaned it up, added sample seed data

--- Excella.Lean.Dal/Migrations/Configuration.cs
+++ Excella.Lean.Dal/Migrations/Configuration.cs
@@ -1,6 +1,9 @@
 namespace Excella.Lean.Dal.Migrations
 {
+    using System;
     using System.Data.Entity.Migrations;
+
+    using Excella.Lean.Core.Models.Shared;
 
     internal sealed class Configuration : DbMigrationsConfiguration<EntityFramework.Contexts.LeanDatabase>
     {
@@ -11,8 +14,23 @@ namespace Excella.Lean.Dal.Migrations
 
         protected override void Seed(EntityFramework.Contexts.LeanDatabase context)
         {
-            //  This method will be called after migrating to the latest version.
-
+            // This method will be called after migrating to the latest version.
+            context.Persons.AddOrUpdate(
+                p => p.Id,
+                new Person
+                    {
+                        FirstName = "John",
+                        LastName = "Doe",
+                        LastUpdateBy = "SYSTEM",
+                        LastUpdateDate = DateTime.Now,
+                    },
+                new Person
+                    {
+                        FirstName = "Jane",
+                        LastName = "Smith",
+                        LastUpdateBy = "SYSTEM",
+                        LastUpdateDate = DateTime.Now,
+                    });
         }
     }
 }
